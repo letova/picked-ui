92ec8192f2ca51d9023343b0515fa576f1ac2cc2
Olga Letova <letova.ov@gmail.com>
2023-09-04 20:05:51 +0300

    fix Paper cs

--- src/pui-base/components/Paper/Paper.tsx
+++ src/pui-base/components/Paper/Paper.tsx
@@ -1,16 +1,20 @@
 import { ForwardedRef, forwardRef } from 'react';
 
 import { PaperProps } from './Paper.types';
-import { cx } from '@emotion/css';
+import { css, cx } from '@emotion/css';
 
 const Paper = forwardRef(
-  ({ className, children, elevation = 1, customStyles }: PaperProps, ref: ForwardedRef<HTMLDivElement>) => {
+  ({ className, children, elevation = 1, cs }: PaperProps, ref: ForwardedRef<HTMLDivElement>) => {
     return (
       <div
         ref={ref}
-        className={cx('Paper', `Paper--elevation${elevation}`, className)}
+        className={cx(
+          'Paper',
+          `Paper--elevation${elevation}`,
+          { [css(cs?.container)]: Boolean(cs?.container) },
+          className,
+        )}
         children={children}
-        style={customStyles?.container}
       />
     );
   },
--- src/pui-base/components/Paper/Paper.types.ts
+++ src/pui-base/components/Paper/Paper.types.ts
@@ -1,12 +1,13 @@
 import React from 'react';
+import { CSSObject } from '@emotion/css';
 
 export interface CustomStyles {
-  container: React.CSSProperties;
+  container: CSSObject;
 }
 
 export interface PaperProps {
   className?: string;
   children?: React.ReactNode;
   elevation?: number;
-  customStyles?: CustomStyles;
+  cs?: CustomStyles;
 }
