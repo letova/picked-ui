e2d16166223732ce8969b6c1bffbb2c57120fe07
Olga Letova <letova.ov@gmail.com>
2023-09-13 19:08:28 +0300

    add sizes map to chip

--- packages/pui-blossom/src/components/Chip/Chip.styles.ts
+++ packages/pui-blossom/src/components/Chip/Chip.styles.ts
@@ -5,9 +5,35 @@ import { deepMergeCS, getPxSize } from '../../utils';
 import { ChipProps } from './Chip.types';
 import { PALETTE_MAP_OUTLINE, PALETTE_MAP_PLAIN } from './Chip.palettes';
 
-export const getClassName = ({ variant = 'plain', color = 'primary', scale: s = 1, onClick }: ChipProps) => {
+const SIZES_MAP = {
+  xs: {
+    paddingX: 6,
+    paddingY: 3,
+    fontSize: 12,
+  },
+  s: {
+    paddingX: 6,
+    paddingY: 3,
+    fontSize: 14,
+  },
+  m: {
+    paddingX: 8,
+    paddingY: 4,
+    fontSize: 16,
+  },
+};
+
+export const getClassName = ({
+  variant = 'plain',
+  size = 's',
+  color = 'primary',
+  scale: s = 1,
+  onClick,
+}: ChipProps) => {
   const paletteSource = variant === 'plain' ? PALETTE_MAP_PLAIN : PALETTE_MAP_OUTLINE;
   const palette = paletteSource[color];
+
+  const sizes = SIZES_MAP[size];
 
   const borderStyle = variant === 'plain' ? 'none' : `${getPxSize(1, s)} solid ${palette.border.normal}`;
 
@@ -31,12 +57,12 @@ export const getClassName = ({ variant = 'plain', color = 'primary', scale: s = 
     display: flex;
     align-items: center;
     gap: ${getPxSize(4, s)};
-    padding: ${getPxSize(3, s)} ${getPxSize(6, s)};
+    padding: ${getPxSize(sizes.paddingY, s)} ${getPxSize(sizes.paddingX, s)};
     border: ${borderStyle};
     border-radius: ${getPxSize(2, s)};
     box-sizing: border-box;
     font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
-    font-size: ${getPxSize(14, s)};
+    font-size: ${getPxSize(sizes.fontSize, s)};
     color: ${palette.text.normal};
     background: ${palette.bg.normal};
     ${hoverStyle}
