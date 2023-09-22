8ed7269c23ecd6c7ff5f144c4ea6b203f8d7e61f
Olga Letova <letova.ov@gmail.com>
2023-09-22 19:50:37 +0300

    add some tests to Checkbox

--- packages/pui-base/src/components/Checkbox/__tests__/index.test.tsx
+++ packages/pui-base/src/components/Checkbox/__tests__/index.test.tsx
@@ -1,14 +1,52 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 
 import { Checkbox } from '../Checkbox';
 
 describe('Checkbox', () => {
+  it('renders an input with the name', () => {
+    render(<Checkbox name="bar" />);
+
+    expect(screen.getByRole('checkbox')).toHaveAttribute('name', 'bar');
+  });
+
+  it('renders an input with the checked state', () => {
+    render(<Checkbox checked />);
+
+    expect(screen.getByRole('checkbox')).toBeChecked();
+  });
+
+  it('renders an input with the default checked state and change it', () => {
+    render(<Checkbox defaultChecked />);
+
+    const input = screen.getByRole('checkbox');
+
+    expect(input).toBeChecked();
+
+    act(() => {
+      input.click();
+    });
+
+    expect(input).not.toBeChecked();
+  });
+
   it('shows label', () => {
-    render(<Checkbox label="test label" checked />);
+    render(<Checkbox label="test label" />);
 
-    const element = screen.queryByText('test label');
+    const label = screen.getByText('test label');
 
-    expect(element).toBeInTheDocument();
+    expect(label).toBeInTheDocument();
+
+    act(() => {
+      label.click();
+    });
+
+    expect(screen.getByRole('checkbox')).toBeChecked();
+  });
+
+  it('the input can be disabled', () => {
+    const { getByRole } = render(<Checkbox disabled />);
+
+    expect(getByRole('checkbox')).toBeDisabled();
   });
 
   it('sets focus to the input', () => {
