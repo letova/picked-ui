56ee74534889132bb8d62629cba8c7e14ff7fbcc
Olga Letova <letova.ov@gmail.com>
2024-01-25 20:10:58 +0300

    fix lint-staged

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   clearMocks: true,
+  roots: ['<rootDir>/packages/base', '<rootDir>/packages/blossom'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|glb)$':
       '<rootDir>/__mocks__/fileMock.js',
   },
+  modulePathIgnorePatterns: ['<rootDir>/packages/.*/dist'],
   setupFilesAfterEnv: ['<rootDir>/tests/setupAfterEnv.ts'],
   testEnvironment: 'jsdom',
 };
