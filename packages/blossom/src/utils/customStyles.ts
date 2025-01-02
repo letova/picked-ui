34a24e9da271802ef901f743158926bc35a9a15a
Olga Letova <letova.ov@gmail.com>
2025-01-02 19:03:05 +0300

    deepMergeCS

--- packages/blossom/src/utils/customStyles.ts
+++ packages/blossom/src/utils/customStyles.ts
@@ -1,10 +1,15 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import merge from 'lodash.merge';
+import { CSSObject } from '@emotion/css';
 
-export const deepMergeCS = (baseStyles?: any, overrideStyles?: any): any => {
+export type CSFn = (state: Record<string, any>) => CSSObject;
+
+export type CSFnParameters = Parameters<CSFn>;
+
+export const deepMergeCS = <T extends Record<string, any>>(
+  baseStyles?: T | undefined,
+  overrideStyles?: T | undefined,
+): T | undefined => {
   if (!baseStyles && !overrideStyles) {
     return undefined;
   }
@@ -17,21 +22,21 @@ export const deepMergeCS = (baseStyles?: any, overrideStyles?: any): any => {
     return overrideStyles;
   }
 
-  const result: any = {};
+  const result: Record<string, any> = {};
 
   for (const key in baseStyles) {
     if (Object.prototype.hasOwnProperty.call(baseStyles, key)) {
-      const baseStyle = baseStyles[key];
-      const overrideStyle = overrideStyles[key];
+      const baseStyle: Record<string, any> | CSFn = baseStyles[key];
+      const overrideStyle: Record<string, any> | CSFn = overrideStyles![key];
 
       if (overrideStyle === undefined) {
         result[key] = baseStyle;
       } else if (typeof baseStyle === 'function' && typeof overrideStyle === 'function') {
-        result[key] = (...args: any[]) => merge(baseStyle(...args), overrideStyle(...args));
+        result[key] = (...args: CSFnParameters) => merge(baseStyle(...args), overrideStyle(...args));
       } else if (typeof baseStyle === 'function' && typeof overrideStyle === 'object') {
-        result[key] = (...args: any[]) => merge(baseStyle(...args), overrideStyle);
+        result[key] = (...args: CSFnParameters) => merge(baseStyle(...args), overrideStyle);
       } else if (typeof baseStyle === 'object' && typeof overrideStyle === 'function') {
-        result[key] = (...args: any[]) => merge(baseStyle, overrideStyle(...args));
+        result[key] = (...args: CSFnParameters) => merge(baseStyle, overrideStyle(...args));
       } else if (typeof baseStyle === 'object' && typeof overrideStyle === 'object') {
         result[key] = deepMergeCS(baseStyle, overrideStyle);
       } else {
@@ -41,10 +46,10 @@ export const deepMergeCS = (baseStyles?: any, overrideStyles?: any): any => {
   }
 
   for (const key in overrideStyles) {
-    if (Object.prototype.hasOwnProperty.call(overrideStyles, key) && !(key in baseStyles)) {
+    if (Object.prototype.hasOwnProperty.call(overrideStyles, key) && !(key in baseStyles!)) {
       result[key] = overrideStyles[key];
     }
   }
 
-  return result;
+  return result as T;
 };
