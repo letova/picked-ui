c77c221b19c0604088c188991ff03a20abac996e
Olga Letova <letova.ov@gmail.com>
2025-01-07 16:20:52 +0300

    checkbox prop description

--- packages/blossom/src/components/Checkbox/Checkbox.types.ts
+++ packages/blossom/src/components/Checkbox/Checkbox.types.ts
@@ -13,13 +13,45 @@ export type CheckboxIconProps = Pick<
 >;
 
 export interface CheckboxProps extends BaseCheckboxProps {
+  /**
+   * The icon when checked is true
+   */
   checkedIcon?: React.ReactNode;
+  /**
+   * The color of the component
+   * @default primary
+   */
   color?: 'primary' | 'secondary' | 'danger' | 'success' | 'warning';
+  /**
+   * The icon when disabled is true
+   */
   disableIcon?: React.ReactNode;
-  focusWrap?: 'input' | 'full';
+  /**
+   * The focus outline wraps target
+   * @default input
+   */
+  focusOutlineWraps?: 'input' | 'full';
+  /**
+   * The icon when the component is indeterminate
+   */
   indeterminateIcon?: React.ReactNode;
+  /**
+   * The scale of the component
+   * @default 1
+   */
   scale?: number;
+  /**
+   * The size of the component
+   * @default s
+   */
   size?: 'xs' | 's' | 'm';
+  /**
+   * The icon when checked is false
+   */
   uncheckedIcon?: React.ReactNode;
-  variant?: 'solid' | 'soft' | 'outlined';
+  /**
+   * The variant of the component
+   * @default solid
+   */
+  variant?: 'soft' | 'solid' | 'outlined';
 }
