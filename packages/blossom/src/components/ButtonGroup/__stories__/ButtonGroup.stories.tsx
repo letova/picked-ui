93e527147a9ca1f6fe00f1dd960c3e607813902b
Olga Letova <letova.ov@gmail.com>
2025-02-16 15:36:34 +0300

    add icon example

--- packages/blossom/src/components/ButtonGroup/__stories__/ButtonGroup.stories.tsx
+++ packages/blossom/src/components/ButtonGroup/__stories__/ButtonGroup.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
+import { Add } from '../../../iconComponents/Add';
+
 import { ButtonGroup } from '../index';
 
 const meta = {
@@ -39,13 +41,24 @@ export const Outlined: Story = {
   },
 };
 
-export const OutlinedConnected: Story = {
+export const WithNoSpacing: Story = {
   render: () => {
     return (
       <ButtonGroup defaultProps={{ variant: 'outlined' }} spacing={0}>
         <ButtonGroup.Button>One</ButtonGroup.Button>
         <ButtonGroup.Button>Two</ButtonGroup.Button>
         <ButtonGroup.Button>Free</ButtonGroup.Button>
+      </ButtonGroup>
+    );
+  },
+};
+
+export const WithIconButton: Story = {
+  render: () => {
+    return (
+      <ButtonGroup defaultProps={{ variant: 'outlined' }} spacing={0}>
+        <ButtonGroup.Button>Text</ButtonGroup.Button>
+        <ButtonGroup.Button startDecorator={<Add fill="currentColor" width={20} height={20} />} />
       </ButtonGroup>
     );
   },
