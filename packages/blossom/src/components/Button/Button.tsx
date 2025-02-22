cd35137a16f0994ee7f415761c86e87e1f62d440
Olga Letova <letova.ov@gmail.com>
2025-02-22 21:41:42 +0300

    fix chip btn

--- packages/blossom/src/components/Button/Button.tsx
+++ packages/blossom/src/components/Button/Button.tsx
@@ -1,13 +1,15 @@
 import { ForwardedRef, forwardRef } from 'react';
 import { Button as BaseButton } from '@picked-ui/base';
 
+import { deepMergeCS } from '../../utils';
+
 import { ButtonProps } from './Button.types';
 import { getCS } from './Button.styles';
 
 const Button = forwardRef((props: ButtonProps, ref: ForwardedRef<HTMLButtonElement>) => {
   const cs = getCS(props);
 
-  return <BaseButton {...props} ref={ref} cs={cs} />;
+  return <BaseButton {...props} ref={ref} cs={props.cs ? deepMergeCS(cs, props.cs) : cs} />;
 });
 
 export { Button };
