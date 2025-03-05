e9712bd28706fb60a8d643aa6ed60541efacb787
Olga Letova <letova.ov@gmail.com>
2025-03-05 20:43:12 +0300

    fix vars

--- packages/base/src/Pagination/__tests__/utils/getCollapsibleRange.test.ts
+++ packages/base/src/Pagination/__tests__/utils/getCollapsibleRange.test.ts
@@ -1,4 +1,4 @@
-import { getCollapsibleRange } from '../../utils';
+import { END_ELLIPSIS, getCollapsibleRange, START_ELLIPSIS } from '../../utils';
 
 describe('Pagination.getCollapsibleRange', () => {
   test('calculates { page: 1, pageCount: 5 }', () => {
@@ -10,24 +10,24 @@ describe('Pagination.getCollapsibleRange', () => {
   test('calculates { page: 1, pageCount: 10 }', () => {
     const result = getCollapsibleRange({ page: 1, pageCount: 10 });
 
-    expect(result).toEqual([1, 2, 3, 4, 5, 'end-ellipsis', 10]);
+    expect(result).toEqual([1, 2, 3, 4, 5, END_ELLIPSIS, 10]);
   });
 
   test('calculates { page: 5, pageCount: 10 }', () => {
     const result = getCollapsibleRange({ page: 5, pageCount: 10 });
 
-    expect(result).toEqual([1, 'start-ellipsis', 4, 5, 6, 'end-ellipsis', 10]);
+    expect(result).toEqual([1, START_ELLIPSIS, 4, 5, 6, END_ELLIPSIS, 10]);
   });
 
   test('calculates { page: 6, pageCount: 12, siblingCount: 2 }', () => {
     const result = getCollapsibleRange({ page: 6, pageCount: 12, siblingCount: 2 });
 
-    expect(result).toEqual([1, 'start-ellipsis', 4, 5, 6, 7, 8, 'end-ellipsis', 12]);
+    expect(result).toEqual([1, START_ELLIPSIS, 4, 5, 6, 7, 8, END_ELLIPSIS, 12]);
   });
 
   test('calculates { page: 2, pageCount: 10, boundaryCount: 0 }', () => {
     const result = getCollapsibleRange({ page: 2, pageCount: 10, boundaryCount: 0 });
 
-    expect(result).toEqual([1, 2, 3, 4, 'end-ellipsis']);
+    expect(result).toEqual([1, 2, 3, 4, END_ELLIPSIS]);
   });
 });
